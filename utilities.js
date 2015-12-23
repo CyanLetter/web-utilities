d045d84451c74654d2b4aac4670b05f4d14c53df
Dakota Ling <ourthinking@gmail.com>
2015-12-23 17:55:11 -0500

    fixed hexToRgb shorthand regex

--- utilities.js
+++ utilities.js
@@ -126,7 +126,7 @@
 	DL_Util.hexToRgb = function (hex) {
 		// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
 		var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-		hex = hex.replace(shorthandRegex, function(r, g, b) {
+		hex = hex.replace(shorthandRegex, function(match, r, g, b) {
 			return r + r + g + g + b + b;
 		});
 
