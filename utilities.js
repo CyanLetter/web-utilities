e0c7cb65eac2bd80108f8a8c3473d9e2ec52a0ee
Dakota Ling <dakota@primalscreen.com>
2016-04-28 14:48:52 -0400

    Added isObjectEmpty method and custom Date formatting code
    Both used in Corkboard

--- utilities.js
+++ utilities.js
@@ -1,5 +1,5 @@
 
-/****************************************************************
+/*******************************************************************
 	This namespacing pattern taken from 
 	http://addyosmani.com/blog/essential-js-namespacing/
 
@@ -9,7 +9,7 @@
 	2. the value of undefined is guaranteed as being truly
 	undefined. This is to avoid issues with undefined being
 	mutable pre-ES5.
- ****************************************************************/
+ *******************************************************************/
 
 (function ( DL_Util, undefined ) {
 
@@ -255,6 +255,22 @@
 		}
 	}
 
+	/*
+	 * Checks to see if properties in an object
+	 * contain any content. Use to make sure 
+	 * object structures that are generated
+	 * as empty templates don't pollute your
+	 * data set with blank entries.
+	 */
+	DL_Util.isObjectEmpty = function (obj) {
+		for(var prop in obj) {
+			if(obj.hasOwnProperty(prop))
+			return false;
+		}
+
+		return true && JSON.stringify(obj) === JSON.stringify({});
+	}
+
 	/****************************************************************
 
 	PARSING
@@ -440,3 +456,33 @@ try {
 	CustomEvent.prototype = window.Event.prototype;
 	window.CustomEvent = CustomEvent; // expose definition to window
 }
+
+/* Date custom format
+ * This code is copyright 2002-2003 by Gavin Kistner, !@phrogz.net
+ * It is covered under the license viewable at http://phrogz.net/JS/_ReuseLicense.txt
+ *
+ * http://stackoverflow.com/questions/4673527/converting-milliseconds-to-a-date-jquery-js
+ *
+ * Usage example:
+ * var date = new Date();
+ * var saneDateString = date.customFormat( "#MM#-#DD#-#YYYY# #hh#:#mm#:#ss#" );
+ */
+Date.prototype.customFormat = function(formatString){
+  var YYYY,YY,MMMM,MMM,MM,M,DDDD,DDD,DD,D,hhhh,hhh,hh,h,mm,m,ss,s,ampm,AMPM,dMod,th;
+  YY = ((YYYY=this.getFullYear())+"").slice(-2);
+  MM = (M=this.getMonth()+1)<10?('0'+M):M;
+  MMM = (MMMM=["January","February","March","April","May","June","July","August","September","October","November","December"][M-1]).substring(0,3);
+  DD = (D=this.getDate())<10?('0'+D):D;
+  DDD = (DDDD=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][this.getDay()]).substring(0,3);
+  th=(D>=10&&D<=20)?'th':((dMod=D%10)==1)?'st':(dMod==2)?'nd':(dMod==3)?'rd':'th';
+  formatString = formatString.replace("#YYYY#",YYYY).replace("#YY#",YY).replace("#MMMM#",MMMM).replace("#MMM#",MMM).replace("#MM#",MM).replace("#M#",M).replace("#DDDD#",DDDD).replace("#DDD#",DDD).replace("#DD#",DD).replace("#D#",D).replace("#th#",th);
+  h=(hhh=this.getHours());
+  if (h==0) h=24;
+  if (h>12) h-=12;
+  hh = h<10?('0'+h):h;
+  hhhh = h<10?('0'+hhh):hhh;
+  AMPM=(ampm=hhh<12?'am':'pm').toUpperCase();
+  mm=(m=this.getMinutes())<10?('0'+m):m;
+  ss=(s=this.getSeconds())<10?('0'+s):s;
+  return formatString.replace("#hhhh#",hhhh).replace("#hhh#",hhh).replace("#hh#",hh).replace("#h#",h).replace("#mm#",mm).replace("#m#",m).replace("#ss#",ss).replace("#s#",s).replace("#ampm#",ampm).replace("#AMPM#",AMPM);
+};
