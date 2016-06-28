bc45c676a8803e051f31fb86200b5345d9541450
Dakota Ling <dakota@primalscreen.com>
2016-06-28 19:49:06 -0400

    Date and Time section
    Move customDate out of Date prototype and into a namespaced function.
    Added pauseable timer function

--- utilities.js
+++ utilities.js
@@ -436,6 +436,71 @@
 		return imageArray;
 	};
 
+	/****************************************************************
+
+	DATE AND TIME
+
+	****************************************************************/
+
+	/*
+	 * setTimeout with pause, resume, and destroy functions
+	 */
+
+	DL_Util.timer = function(callback, delay) {
+		var timerId, start, remaining = delay;
+
+		this.pause = function() {
+			window.clearTimeout(timerId);
+			remaining -= new Date() - start;
+		};
+
+		this.resume = function() {
+			start = new Date();
+			window.clearTimeout(timerId);
+			timerId = window.setTimeout(callback, remaining);
+		};
+
+		this.destroy = function() {
+			window.clearTimeout(timerId);
+		}
+
+		this.resume();
+	};
+
+	/* Date custom format
+	 * This code is copyright 2002-2003 by Gavin Kistner, !@phrogz.net
+	 * It is covered under the license viewable at http://phrogz.net/JS/_ReuseLicense.txt
+	 *
+	 * http://stackoverflow.com/questions/4673527/converting-milliseconds-to-a-date-jquery-js
+	 *
+	 * Usage example:
+	 * var saneDateString = DL_Util.customDate("#MM#-#DD#-#YY# #hh#:#mm# #AMPM#");
+	 */
+	DL_Util.customDate = function(formatString, date){
+		var YYYY, YY, MMMM, MMM, MM, M, DDDD, DDD, DD, D, hhhh, hhh, hh, h, mm, m, ss, s, ampm, AMPM, dMod, th, date = new Date();
+
+		YY = ((YYYY = date.getFullYear())+"").slice(-2);
+		MM = (M = date.getMonth() + 1) < 10 ? ('0' + M) : M;
+		MMM = (MMMM = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][M - 1]).substring(0, 3);
+		DD = (D = date.getDate()) < 10 ? ('0' + D) : D;
+		DDD = (DDDD = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][date.getDay()]).substring(0, 3);
+		th = (D >= 10 && D <= 20 ) ? 'th' : ((dMod = D % 10) == 1) ? 'st' : (dMod == 2) ? 'nd' : (dMod == 3) ? 'rd' : 'th';
+		formatString = formatString.replace("#YYYY#", YYYY).replace("#YY#", YY).replace("#MMMM#", MMMM).replace("#MMM#", MMM).replace("#MM#", MM).replace("#M#", M).replace("#DDDD#", DDDD).replace("#DDD#", DDD).replace("#DD#", DD).replace("#D#", D).replace("#th#", th);
+		h=(hhh = date.getHours());
+		if (h === 0) {
+			h = 24;
+		}
+		if (h > 12) {
+			h -= 12;
+		}
+		hh = h < 10 ? ('0' + h) : h;
+		hhhh = h < 10 ? ('0' + hhh) : hhh;
+		AMPM = (ampm = hhh < 12 ? 'am' : 'pm').toUpperCase();
+		mm = (m = date.getMinutes()) < 10 ? ('0' + m) : m;
+		ss = (s = date.getSeconds()) < 10 ? ('0' + s) : s;
+		return formatString.replace("#hhhh#", hhhh).replace("#hhh#", hhh).replace("#hh#", hh).replace("#h#", h).replace("#mm#", mm).replace("#m#", m).replace("#ss#", ss).replace("#s#", s).replace("#ampm#", ampm).replace("#AMPM#", AMPM);
+	};
+
 })(window.DL_Util = window.DL_Util || {});
 
 /****************************************************************
@@ -510,38 +575,3 @@ try {
 	CustomEvent.prototype = window.Event.prototype;
 	window.CustomEvent = CustomEvent; // expose definition to window
 }
-
-/* Date custom format
- * This code is copyright 2002-2003 by Gavin Kistner, !@phrogz.net
- * It is covered under the license viewable at http://phrogz.net/JS/_ReuseLicense.txt
- *
- * http://stackoverflow.com/questions/4673527/converting-milliseconds-to-a-date-jquery-js
- *
- * Usage example:
- * var date = new Date();
- * var saneDateString = date.customFormat( "#MM#-#DD#-#YYYY# #hh#:#mm#:#ss#" );
- */
-Date.prototype.customFormat = function(formatString){
-	var YYYY, YY, MMMM, MMM, MM, M, DDDD, DDD, DD, D, hhhh, hhh, hh, h, mm, m, ss, s, ampm, AMPM, dMod, th;
-
-	YY = ((YYYY = this.getFullYear())+"").slice(-2);
-	MM = (M = this.getMonth() + 1) < 10 ? ('0' + M) : M;
-	MMM = (MMMM = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][M - 1]).substring(0, 3);
-	DD = (D = this.getDate()) < 10 ? ('0' + D) : D;
-	DDD = (DDDD = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][this.getDay()]).substring(0, 3);
-	th = (D >= 10 && D <= 20 ) ? 'th' : ((dMod = D % 10) == 1) ? 'st' : (dMod == 2) ? 'nd' : (dMod == 3) ? 'rd' : 'th';
-	formatString = formatString.replace("#YYYY#", YYYY).replace("#YY#", YY).replace("#MMMM#", MMMM).replace("#MMM#", MMM).replace("#MM#", MM).replace("#M#", M).replace("#DDDD#", DDDD).replace("#DDD#", DDD).replace("#DD#", DD).replace("#D#", D).replace("#th#", th);
-	h=(hhh = this.getHours());
-	if (h === 0) {
-		h = 24;
-	}
-	if (h > 12) {
-		h -= 12;
-	}
-	hh = h < 10 ? ('0' + h) : h;
-	hhhh = h < 10 ? ('0' + hhh) : hhh;
-	AMPM = (ampm = hhh < 12 ? 'am' : 'pm').toUpperCase();
-	mm = (m = this.getMinutes()) < 10 ? ('0' + m) : m;
-	ss = (s = this.getSeconds()) < 10 ? ('0' + s) : s;
-	return formatString.replace("#hhhh#", hhhh).replace("#hhh#", hhh).replace("#hh#", hh).replace("#h#", h).replace("#mm#", mm).replace("#m#", m).replace("#ss#", ss).replace("#s#", s).replace("#ampm#", ampm).replace("#AMPM#", AMPM);
-};
