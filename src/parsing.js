9b544b86f833f70a9602541078a995ecb2c6db7e
Dakota Ling <dakota@primalscreen.com>
2016-11-03 10:26:16 -0400

    Added getBracketedContent
    First pass of a function to get the contents of a bracketed string,
    such as a function body

--- src/parsing.js
+++ src/parsing.js
@@ -55,7 +55,86 @@
 	 * at the moment, so should probably 
 	 * validate that input equals output
 	 */
-	DL_.getFileType = function (url) {
+	DL_.getFileType = function(url) {
 		var fileType = url.split('?').shift().split('.').pop().toLowerCase();
 		return fileType;
+	};
+
+	/*
+	 * Get the contents of a function body
+	 * or other bracketed content, based off of 
+	 * an index or search string.
+	 */
+	DL_.getBracketedContent = function(data, start, options) {
+		var firstIndex = 0;
+		var lastIndex = data.length;
+		var foundFirstBracket = false;
+		var bracketCount = 0;
+		var openChar = "{";
+		var closeChar = "}";
+		var inclusive = true;
+
+		// validate inputs
+		if (typeof start === "number") {
+			// this is an index
+			firstIndex = parseInt(start);
+		} else if (typeof start === "string") {
+			// this is a string to search for, get the index ourselves
+			firstIndex = data.indexOf(start);
+		} else {
+			// this is invalid, exit
+			console.error("Index " + index + " must be of type string or number");
+			return {
+				"start": firstIndex,
+				"end": lastIndex,
+				"contents": ""
+			};
+		}
+
+		if (typeof options !== "undefined") {
+			if (typeof options.openChar !== "undefined") {
+				openChar = options.openChar;
+			}
+			if (typeof options.closeChar !== "undefined") {
+				closeChar = options.closeChar;
+			}
+			if (typeof options.inclusive !== "undefined") {
+				inclusive = options.inclusive;
+			}
+		}
+
+		for (var i = firstIndex; i < data.length; i++) {
+			var curChar = data.charAt(i);
+			if (curChar == openChar) {
+				bracketCount++;
+
+				if (!foundFirstBracket) {
+					// this is the first bracket
+					if (!inclusive) {
+						// don't include brackets if not inclusive
+						firstIndex = i + 1;
+					}
+					foundFirstBracket = true;
+				}
+			} else if (curChar == closeChar) {
+				bracketCount--;
+			}
+
+			if (foundFirstBracket) {
+				if (bracketCount <= 0) {
+					if (!inclusive) {
+						lastIndex = i;
+					} else {
+						lastIndex = (i + 1);
+					}
+					break;
+				}
+			}
+		}
+
+		return {
+			"start": firstIndex,
+			"end": lastIndex,
+			"contents": data.slice(firstIndex, lastIndex)
+		};
 	};
