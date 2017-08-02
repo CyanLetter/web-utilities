872de7eb6e48b3eaa6adf7c519cb9f3098541a24
Dakota Ling <ourthinking@gmail.com>
2017-08-02 00:14:04 -0400

    Adding code coverage through istanbul, codecov
    istanbul works, committing to test codecov integration

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,17 +48,35 @@ module.exports = function(grunt) {
 			}
 		},
 		jasmine: {
-			src : 'build/utilities.js',
-			options : {
-				specs : 'spec/**/*.js',
-				page: {
-					settings: {
-						userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13F69 Safari/601.1'
+			all: {
+				src : 'build/utilities.js',
+				options : {
+					specs : 'spec/**/*.js',
+					page: {
+						settings: {
+							userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13F69 Safari/601.1'
+						}
+					},
+					vendor: [
+						"https://code.jquery.com/jquery-3.1.0.min.js"
+					]
+				}
+			},
+			istanbul: {
+				src: '<%= jasmine.all.src %>',
+				options: {
+					vendor: '<%= jasmine.all.options.vendor %>',
+					specs: '<%= jasmine.all.options.specs %>',
+					page: '<%= jasmine.all.options.page %>',
+					template: require('grunt-template-jasmine-istanbul'),
+					templateOptions: {
+						coverage: 'coverage/json/coverage.json',
+						report: [
+							{type: 'html', options: {dir: 'coverage/html'}},
+							{type: 'text-summary'}
+						]
 					}
-				},
-				vendor: [
-					"https://code.jquery.com/jquery-3.1.0.min.js"
-				]
+				}
 			}
 		}
 	});
@@ -72,5 +90,6 @@ module.exports = function(grunt) {
 	grunt.registerTask('default', ['concat', 'jshint:dist', 'uglify', 'jsdoc']);
 	grunt.registerTask('dev', ['concat', 'jshint:dist']);
 	grunt.registerTask('build', ['concat', 'jshint:dist', 'uglify', 'jsdoc']);
-	grunt.registerTask('test', ['concat', 'jshint:dist', 'jasmine']);
+	grunt.registerTask('test', ['concat', 'jshint:dist', 'jasmine:all']);
+	grunt.registerTask('coverage', ['concat', 'jshint:dist', 'jasmine:istanbul']);
 };
